bb6de499664dbbf9899f0fd184be1c6e12de8bd5
dhruv-chitkara27 <32064824+dhruv-chitkara27@users.noreply.github.com>
2018-08-09 22:57:43 +0530

    Update sum_lecture.py

--- sum_lecture.py
+++ sum_lecture.py
@@ -1,4 +1,4 @@
-a = int(input("Enter the value of a"))
-b = int(input("Enter the vaue of b"))
+a = int(input("Enter the value of a="))
+b = int(input("Enter the vaue of b="))
 sum = int(a)+int(b)
 print(sum)
