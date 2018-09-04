dce940f05bb1e03c3a2f9333ce5aa26ba6ed995f
dhruv-chitkara <dhruvchitkara27@gmail.com>
2018-09-04 10:29:51 +0530

    added more questions

---
+++ trace_robot_path.py
@@ -0,0 +1,24 @@
+"""
+
+A robot moves in a plane starting from the origin point (0,0). The robot can move UP , DOWN , LEFT , RIGHT with a given steps. The trace of the robot movement is shown as: 
+ 			   UP 5 
+ 			   DOWN 3 
+ 			   LEFT 3 
+ 			   RIGHT 2  
+ 			   i->number
+
+The number after the directions  are the steps. Write a program to compute the distance from the current position after a sequence of movement and the original point. If the distance is a float then print the nearest integer.
+
+SAMPLE INPUT :
+               UP 5
+               DOWN 3
+ 	       LEFT 3
+ 	       RIGHT 2
+
+
+SAMPLE OUTPUT :
+               2  
+
+"""
+
+
