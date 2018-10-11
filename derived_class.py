aba92657f6db66d2ff990027f7cd5a95e96cb597
dhruvchitkara <33182227+dhruvchitkara@users.noreply.github.com>
2018-10-11 15:53:19 +0530

    Add files via upload

---
+++ derived_class.py
@@ -0,0 +1,11 @@
+
+"""
+
+Q-2 -->> Write a program that has a class distance with members kms and metres . Derived classes school and office which stores distance of house to school and distnace of house to office and the speed the sameq2 -- Write a program that has a class distance with members kms and metres . Derived classes school and office which stores distance of house to school and distnace of house to office and the speed the same
+
+"""
+class Distance:
+	def __init__(self,kms,metres):
+		self.kms = kms.
+		self.metres = metres
+		
